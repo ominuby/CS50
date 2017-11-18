50a192963d01bf554cfda5437e0054a408f67fa6
ominuby <oimnub@gmail.com>
2017-11-18 22:15:17 +0800

    finish resize

--- pset4/resize/resize.c
+++ pset4/resize/resize.c
@@ -35,9 +35,10 @@ int main(int argc, char* argv[])
         }
 
     // parse float information
-    float factor = atof (argv[1]);
-    printf("%f\n",factor);
+    int factor = atoi (argv[1]);
+
     // the size is corresponding to input argument
+
         // read infile's BITMAPFILEHEADER
         BITMAPFILEHEADER bf;
         fread(&bf, sizeof(BITMAPFILEHEADER), 1, inptr);
@@ -55,53 +56,64 @@ int main(int argc, char* argv[])
             fprintf(stderr, "Unsupported file format.\n");
             return 4;
         }
-        // new size of bitmap
+
+        // outfile's BITMAPFILEHEADER
+        BITMAPFILEHEADER bf_out = bf;
+
+        // outfile's BITMAPINFOHEADER
+        BITMAPINFOHEADER bi_out = bi;
+
+        // new length and width of bitmap
         int newWidth = bi.biWidth * factor;
-        int newHeight = abs(bi.biHeight) * factor;
+//        int newHeight = abs(bi.biHeight) * factor;
+
+        bi_out.biWidth = bi.biWidth * factor;
+        bi_out.biHeight = bi.biHeight * factor;
+
+        // determine padding for scanlines
+        int padding_old = (4 - (bi.biWidth * sizeof(RGBTRIPLE)) % 4) % 4;
+        int padding =  (4 - (newWidth * sizeof(RGBTRIPLE)) % 4) % 4;
+
+        bi_out.biSizeImage = ((sizeof(RGBTRIPLE) * bi_out.biWidth + padding) * abs(bi_out.biHeight));
+        bf_out.bfSize = bi_out.biSizeImage + sizeof(BITMAPFILEHEADER) + sizeof(BITMAPINFOHEADER);
+
         // write outfile's BITMAPFILEHEADER
-        fwrite(&bf,  sizeof(BITMAPFILEHEADER), 1, outptr);
+        fwrite(&bf_out,  sizeof(BITMAPFILEHEADER), 1, outptr);
 
         // write outfile's BITMAPINFOHEADER
-        fwrite(&bi, sizeof(BITMAPINFOHEADER), 1, outptr);
+        fwrite(&bi_out, sizeof(BITMAPINFOHEADER), 1, outptr);
 
-    // determine padding for scanlines
-    int padding =  (4 - (newWidth * sizeof(RGBTRIPLE)) % 4) % 4;
+
 
     //Resize
-    // enlarge
-//    if (factor >= 1)
-//    {
-        for (int i = 0; i < newHeight; i++)
+        for (int i = 0; i < abs(bi.biHeight); i++)
         {
-          // iterate over pixels in scanline
-            for (int j = 0; j < newWidth; j++)
-           {
-                // temporary storage
-                RGBTRIPLE triple;
-
-                // read RGB triple from infile
-                if (i % (int)factor == 0 && j % (int)factor == 0)
-                {
-                fread(&triple, sizeof(RGBTRIPLE), 1, inptr);
-                }
-                // write RGB triple to outfile
-                fwrite(&triple, sizeof(RGBTRIPLE), 1, outptr);
-            }
-
-            // skip over padding, if any
-            fseek(inptr, padding, SEEK_CUR);
-
-            // then add it back (to demonstrate how)
-            for (int k = 0; k < padding; k++)
+            //height pixel of new figure
+            for (int h = 0; h < factor; h++)
             {
-                fputc(0x00, outptr);
+                // search appropriate position for loading
+                fseek(inptr, sizeof(BITMAPFILEHEADER) + sizeof(BITMAPINFOHEADER) + (bi.biWidth * 3 + padding_old) * i, SEEK_SET);
+                for (int j = 0; j < bi.biWidth; j++)
+                {
+                    // temporary storage
+                    RGBTRIPLE triple;
+                    // read RGB triple from infile
+                    fread(&triple, sizeof(RGBTRIPLE), 1, inptr);
+
+                    // width pixel of new figure
+                    for (int w = 0; w < factor; w++)
+                    {
+                        // write RGB triple to outfile
+                        fwrite(&triple, sizeof(RGBTRIPLE), 1, outptr);
+                    }
+                }
+                // then add it back (to demonstrate how)
+                for (int k = 0; k < padding; k++)
+                {
+                    fputc(0x00, outptr);
+                }
             }
         }
-//    }
-//    else // shrink
-//    {
-
-//    }
 
     //closing files
     fclose(inptr);
