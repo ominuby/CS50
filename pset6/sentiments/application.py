65c62326d398b67648c7843139d3da01c2dd94e8
ominuby <oimnub@gmail.com>
2017-12-11 15:04:31 +0800

    finish pset6

--- pset6/sentiments/application.py
+++ pset6/sentiments/application.py
@@ -1,5 +1,6 @@
 from flask import Flask, redirect, render_template, request, url_for
-
+import os
+import sys
 import helpers
 from analyzer import Analyzer
 
@@ -20,9 +21,29 @@ def search():
     # get screen_name's tweets
     tweets = helpers.get_user_timeline(screen_name)
 
-    # TODO
-    positive, negative, neutral = 0.0, 100.0, 0.0
+    # redirect to index if screen name is private or doesn't exist
+    if tweets == None:
+        return redirect(url_for("index"))
 
+    # absolute paths to lists
+    positives = os.path.join(sys.path[0], "positive-words.txt")
+    negatives = os.path.join(sys.path[0], "negative-words.txt")
+    analyzer = Analyzer(positives, negatives)
+
+    positive, negative, neutral = 0.0, 0.0, 0.0
+
+    for tweet in tweets:
+        score = analyzer.analyze(tweet)
+        if score > 0:
+            positive = positive + 1.0
+        elif score < 0:
+            negative = negative + 1.0
+        else:
+            neutral = neutral + 1.0
+    cal = positive + negative + neutral
+    positive = positive / cal
+    negative = negative / cal
+    neutral = neutral / cal
     # generate chart
     chart = helpers.chart(positive, negative, neutral)
 
